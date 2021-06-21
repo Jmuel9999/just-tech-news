173caacb7e9116ba50a47ccd320d3622920b83b0
jasonmueller <jmuel9999@gmail.com>
2021-06-20 20:28:52 -0500

    Finished mod 13.2.6

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,32 @@ router.post('/', (req, res) => {
       });
   });
 
+// Log in route
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    // "Find the email from the request..."
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });  
+}); 
+
 // PUT /api/users/1
 // UPDATE users
 // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
@@ -64,6 +90,8 @@ router.put('/:id', (req, res) => {
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+      // imports the hooks from bcrypt on User.js page, this is used when updating a user's password
+      individualHooks: true,
       where: {
         id: req.params.id
       }
