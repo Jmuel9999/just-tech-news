55c99366409b97e0ec1ffd5701f3eacd997db7ef
jasonmueller <jmuel9999@gmail.com>
2021-06-21 11:30:02 -0500

    finished 13.3

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// this adds the constraint that a post can belong to one user, but not many users
+Post.belongsTo(User, {
+foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
---
+++ routes/api/post-routes.js
@@ -0,0 +1,114 @@
+const router = require('express').Router();
+// we included the User model AND Post model because we want to retrieve info about
+//    each post, but also the user that posted it
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        // ensures that the latest posted articles show first
+        order: [['created_at', 'DESC']], 
+        include: [
+        {
+            model: User,
+            attributes: ['username']
+        }
+      ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+});
+
+// get one user
+router.get('/:id', (req, res) => {
+Post.findOne({
+    where: {
+    id: req.params.id
+    },
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+    {
+        model: User,
+        attributes: ['username']
+    }
+  ]
+})
+    .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+// creates post
+router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// updates post
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// delete an entry
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// keep this expression at bottom of file, we want to assign 'router' once Express API endpoints have been defined
+module.exports = router;
